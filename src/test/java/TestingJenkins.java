7ae8522504de5ba8a2c459d6ff54972737713c23
SaiTharun25 <saitharunchandanala@gmail.com>
2026-02-21 18:17:56 +0530

    Update TestingJenkins.java

--- src/test/java/TestingJenkins.java
+++ src/test/java/TestingJenkins.java
@@ -1,6 +1,6 @@
 package TestingJenkins;
 import org.testng.annotations.*;
-public class Test1 {
+public class TestingJenkins {
 	@Test
 	public static void run() {
 		System.out.println("Sai Jenkins 1");
